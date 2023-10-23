c6707dbe30f1f39f75a9dfec0b0a5fed5f40dfe4
dylewski <130579182+dylewskii@users.noreply.github.com>
2023-10-23 17:28:31 +0200

    add createGameboard, createPlayers & playRound function

--- app.js
+++ app.js
@@ -1,35 +1,27 @@
-// Gameboard Object
+// store the gameboard as an array 
+// inside of a Gameboard object.
 
-// You’re going to store the gameboard as an array inside 
-// of a Gameboard object, so start there! 
+const createGameboard = (function() {
+    const Gameboard = {};
+    Gameboard.gameboard = [];
 
-// Your players are also going to be stored in objects,
-// and you’re probably going to want an object 
-// to control the flow of the game itself.
+    return Gameboard;
+})();
 
-// Your main goal here is to have as little global code as 
-// possible. 
+// players are going to be stored in objects
+function createPlayers(nameOne, nameTwo) {
+    const players = {
+        playerOne: {},
+        playerTwo: {}
+    };
 
-// Try tucking everything away inside of a module, 
-// or factory. 
+    players.playerOne.name = nameOne;
+    players.playerTwo.name = nameTwo;
 
-// Rule of thumb: if you only ever need ONE of something 
-// (gameBoard, displayController), use a module. 
-
-// If you need multiples of something (players!), 
-// create them with factories.
-
-function createGameboard() {
-    return {
-        gameboard: [1, 2, 3, 4, 5, 6, 7, 8, 9]
-    }
+    return players;
 };
 
-function createPlayer() {
-    const players = {};
-    
-    return players;
-}
+// object to control the flow of the game itself.
 
 function playRound(playerChoice, computerChoice) {
     const round = {};
@@ -37,21 +29,8 @@ function playRound(playerChoice, computerChoice) {
     round.playerChoice = playerChoice;
     round.computerChoice = computerChoice;
 
-    round.playerScore = player
+    round.playerScore = 0;
+    round.computerScore = 0;
 
     return round;
-}
-
-function displayController() {
-    renderGameBoard() {
-        //
-    }
-
-    updateScreen(){
-        //
-    }
-
-    updateScore(){
-        //
-    }
 };
