11f5ba76d48549d3d4ab59e1946bb322ddc0e916
dylewski <130579182+dylewskii@users.noreply.github.com>
2023-10-23 22:53:50 +0200

    add resetDisplay function

--- app.js
+++ app.js
@@ -1,13 +1,15 @@
 // Gameboard object which stores the gameboard array
 const createGameboard = (function() {
     const Gameboard = {};
+
     Gameboard.gameboard = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+    Gameboard.getBoard = () => gameboard;
 
     return Gameboard;
 })();
 
 // players are going to be stored in objects
-function createPlayers(nameOne, nameTwo) {
+const createPlayers = function(nameOne, nameTwo) {
     const players = {
         playerOne: {},
         playerTwo: {}
@@ -20,7 +22,7 @@ function createPlayers(nameOne, nameTwo) {
 };
 
 // object to control the flow of the game itself.
-function playRound(playerChoice, computerChoice) {
+const playRound = function(playerChoice, computerChoice) {
     const round = {};
 
     round.playerChoice = playerChoice;
@@ -34,22 +36,29 @@ function playRound(playerChoice, computerChoice) {
 
 // render the contents of the gameboard array to the webpage 
 const displayController = (function() {
-    
-    function createDisplay() {
-        const gridContainer = document.querySelector(".grid-container");
-        const gameboard = createGameboard.gameboard;
+    const gridContainer = document.querySelector(".grid-container");
+    const gameboard = createGameboard.gameboard;
 
+    function createDisplay() {
         for (let square = 0; square < gameboard.length; square++){
             const newSquare = document.createElement("div");
             newSquare.classList.add("grid-box");
             newSquare.textContent = gameboard[square];
             gridContainer.appendChild(newSquare);
         }
-    }
+    };
+
+    function resetDisplay() {
+        const gameboardSquares = document.querySelectorAll(".grid-box")
+        gameboardSquares.forEach(square => {
+            square.innerText = "";
+        });
+    };
 
     return {
-        createDisplay
+        createDisplay,
+        resetDisplay
     }
 })();
 
-displayController.createDisplay()
+displayController.createDisplay();
